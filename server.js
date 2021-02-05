7dbb53f671ca4cc51248b9572a29e28dbde427e7
Thereza Bandoli <tcbandolilegg@gmail.com>
2021-02-04 22:20:47 -0300

    Finalizando as rotas e controlers

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const app = express()
 const port = 8080
-const router = require("./src/routes/index.js")
+
+
 app.listen(port, () => {
   console.log(`servidor subiu na porta ${port}`)
 })
 
 const index = require("./src/routes/index")
+const pessoas = require("./src/routes/pessoasRouters")
 
 app.use("/", index)
+app.use("/pessoas", pessoas)
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,10 +2,12 @@ const express = require("express");
 const router = express.Router()
 
 
-router.get("/", (req, res) => {
+router.get("/", (_, res) => {
 
   res.send("Hello word")
 })
+
+
 
 
 module.exports = router 
